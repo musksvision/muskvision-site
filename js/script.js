d56490a4fdbf9325c0da7115497a479ab8add721
muskvision <musksvision@gmail.com>
2021-09-22 01:46:54 +0530

    Auto hide collapse, transactions table padding

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(".navbar-nav li a").click(function(){
     $(".navbar-nav li a").removeClass("active");
     $(this).addClass("active");
+    $(".navbar-collapse").collapse("hide");
   });
 
 function copyToClipboard() {
