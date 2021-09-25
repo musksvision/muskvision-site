960c9cc00f68d941f28ad787c36f95e030c73c31
musksvision <90548263+musksvision@users.noreply.github.com>
2021-09-25 22:26:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if (window.location.href.split("#").length >= 2) {
   var div_url = "#" + window.location.href.split("#")[1];
-  scroll_to(div_url, 100, flag=true);
+  scroll_to(div_url, 90, flag=true);
 }
 
 function scroll_to(clicked_link, nav_height, flag=false) {
