51d731c5e79c2a65829dbce79e85864503960518
muskvision <musksvision@gmail.com>
2021-09-25 21:43:41 +0530

    Background images check

--- js/script.js
+++ js/script.js
@@ -18,18 +18,6 @@ $('a.scroll-link').on('click', function(e) {
     scroll_to($(this), 90);
 });
 
-function copyToClipboard() {
-
-    var copyText = document.getElementById("mv-contract-address");
-
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-
-    navigator.clipboard.writeText(copyText.value);
-    console.log("COPIED " + copyText.value);
-}
-
-
 function copyText() {
     var $temp = $("<input>");
     var $url = $("#mv-contract-address").text().replace("                       ", "");
